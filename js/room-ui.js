39f0c1f64ac058a269d341be0c3a9a0adfaa7e89
workshopjwl <workshop.jwl@gmail.com>
2026-03-14 15:08:04 +0800

    Update room-ui.js

--- js/room-ui.js
+++ js/room-ui.js
@@ -1,5 +1,5 @@
-import * as THREE from "https://unpkg.com/three@0.160.0/build/three.module.js";
-import { OrbitControls } from "https://unpkg.com/three@0.160.0/examples/jsm/controls/OrbitControls.js";
+import * as THREE from "three"; 
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
 const canvas = document.getElementById("roomCanvas");
 const labelLayer = document.getElementById("labelLayer");
